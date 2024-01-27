106840d92c7dcc174f191c2328d4ffb835983796
mizmauz <krammer@heureso.com>
2024-01-27 11:30:46 +0100

    Import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,21 @@
+-- Movein 2020
+INSERT INTO meterreading (id, errorcode, meternumber, reading_date, reading_time, readingsource, readingreason, unitofreading, readingamount, readingstatus) VALUES ( 1, 0, 'ISK12345', '2020-01-01', '06:00:00', 0, 0, 0, '0.00', 0);
+-- Intermediate 2020
+INSERT INTO meterreading (id, errorcode, meternumber, reading_date, reading_time, readingsource, readingreason, unitofreading, readingamount, readingstatus) VALUES ( 2, 0, 'ISK12345', '2020-05-30', '06:00:00', 0, 3, 0, '450.00', 0);
+-- Billing 2021
+INSERT INTO meterreading (id, errorcode, meternumber, reading_date, reading_time, readingsource, readingreason, unitofreading, readingamount, readingstatus) VALUES ( 3, 0, 'ISK12345', '2021-01-01', '06:00:00', 0, 2, 0, '1300.00', 0);
+-- Billing 2022
+INSERT INTO meterreading (id, errorcode, meternumber, reading_date, reading_time, readingsource, readingreason, unitofreading, readingamount, readingstatus) VALUES ( 4, 0, 'ISK12345', '2022-01-01', '06:00:00', 0, 2, 0, '2888.00', 0);
+
+-- Movein 2020
+INSERT INTO meterreading (id, errorcode, meternumber, reading_date, reading_time, readingsource, readingreason, unitofreading, readingamount, readingstatus) VALUES ( 5, 0, 'ISK1', '2020-01-01', '06:00:00', 0, 0, 0, '0.00', 0);
+-- Intermediate 2020
+INSERT INTO meterreading (id, errorcode, meternumber, reading_date, reading_time, readingsource, readingreason, unitofreading, readingamount, readingstatus) VALUES ( 6, 0, 'ISK1', '2020-05-30', '06:00:00', 0, 3, 0, '450.00', 0);
+-- Billing 2021
+INSERT INTO meterreading (id, errorcode, meternumber, reading_date, reading_time, readingsource, readingreason, unitofreading, readingamount, readingstatus) VALUES ( 7, 0, 'ISK1', '2021-01-01', '06:00:00', 0, 2, 0, '1300.00', 0);
+-- Billing 2022
+INSERT INTO meterreading (id, errorcode, meternumber, reading_date, reading_time, readingsource, readingreason, unitofreading, readingamount, readingstatus) VALUES ( 9, 0, 'ISK1', '2022-01-01', '06:00:00', 0, 2, 0, '2888.00', 0);
+
+
+-- Movein# Dataset Only Movein Exists
+INSERT INTO meterreading (id, errorcode, meternumber, reading_date, reading_time, readingsource, readingreason, unitofreading, readingamount, readingstatus) VALUES ( 10, 0, 'ISK12346', '2020-01-01', '06:00:00', 0, 0, 0, '0.00', 0);
--- target/generated-sources/annotations/com/heureso/mapping/MeterReadingMapperImpl.java
+++ target/generated-sources/annotations/com/heureso/mapping/MeterReadingMapperImpl.java
@@ -7,7 +7,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-01-14T20:53:10+0100",
+    date = "2024-01-20T15:03:00+0100",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.3 (Eclipse Adoptium)"
 )
 @Singleton
